fce1dca4db41a7d0c90b706849439ad76b846a75
ujjwal shekdar <ujjwalshekdar@gmail.com>
2023-08-12 14:21:08 +0530

    crud operations

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const jsonServer = require('json-server');
+const path = require('path');
+
+const app = express();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+
+const PORT = process.env.PORT || 3000;
+
+app.use('/api', router);
+app.use(middlewares);
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
